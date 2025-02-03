b03162943a9a1039d087bb50b1de6febb63ad0ef
nhattvim <nhattruong13112000@gmail.com>
2025-02-03 14:58:54 +0000

    first commit

---
+++ user.js
@@ -0,0 +1,37 @@
+// Default rules
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("widget.gtk.rounded-bottom-corners.enabled", true);
+user_pref("widget.gtk.ignore-bogus-leave-notify", 1);
+user_pref("layout.css.color-mix.enabled", true);
+user_pref("layout.css.has-selector.enabled", true);
+user_pref("layout.css.prefers-color-scheme.content-override", 2);
+user_pref("layout.css.backdrop-filter.enabled", true);
+user_pref("browser.tabs.delayHidingAudioPlayingIconMS", 0);
+user_pref("browser.urlbar.suggest.calculator", true);
+user_pref("browser.urlbar.unitConversion.enabled", true);
+user_pref("browser.toolbars.bookmarks.showOtherBookmarks", true);
+user_pref("browser.urlbar.trimHttps", true);
+user_pref("browser.urlbar.trimURLs", true);
+user_pref(
+    "browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar",
+    false,
+);
+user_pref(
+    "browser.newtabpage.activity-stream.newtabWallpapers.v2.enabled",
+    true,
+);
+
+// To active container tabs without any extension
+user_pref("privacy.userContext.enabled", true);
+user_pref("privacy.userContext.ui.enabled", true);
+user_pref("privacy.userContext.longPressBehavior", 2);
+
+// Enable DevTools on firefox
+user_pref("devtools.chrome.enabled", true);
+user_pref("devtools.debugger.remote-enabled", true);
+user_pref("devtools.debugger.prompt-connection", false);
+
+// Settings
+user_pref("firefoxone.selected_tab_gradient_border", true);
+user_pref("firefoxone.image_in_menus", true);
